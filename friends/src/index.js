0c5ed97958b35121fbb23900dc40a59d59fc9f3e
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-10 17:52:38 -0400

    Completes project initialization steps

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk, logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+  friends: [],
+}
+
+export default const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
