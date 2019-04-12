46358f433b3e9b56e6b9a499cb405d1db515c7c1
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-11 21:03:25 -0400

    Adds Link in FriendsList to '/add-friend'

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getFriends } from '../actions';
+import { Link } from 'react-router-dom';
 
 class FriendsList extends React.Component {
   componentDidMount() {
@@ -15,6 +16,9 @@ class FriendsList extends React.Component {
           ? <h3>Loading Friends...</h3>
           : <ul className='friends-list'>
               {this.props.friends.map(friend => <li key={friend.id}>{friend.name} ({friend.age}): {friend.email}</li>)}
+              <Link to="/add-friend">
+                Add Friend
+              </Link>
             </ul>}
       </div>
     );
